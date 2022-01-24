0025eca48dcccc680b340c791d3da80f8b8dd0ca
Sriram Nair <shnair1995@gmail.com>
2022-01-24 12:19:33 -0600

    Test

---
+++ HelloJava.java
@@ -0,0 +1,15 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        int people = 20;
+        people = people - 20;
+        System.out.println(people);
+
+        String sentence = "Num of passengers:";
+        System.out.println(sentence);
+        System.out.println("Hello " + 50);
+
+        char hello = 'a';
+    }
+}
